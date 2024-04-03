ea574cd65fe7cf9866e99e0c7ca69915aa8068fb
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-04-02 20:19:27 -0700

    Reset example

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,13 @@ import "./App.css";
 
 function App() {
 
-  const [counter, setCounter] = useState(0);
+ const [counter, setCounter] = useState(0);
 
-  useEffect(() => {
- const key = setInterval(() => {
-    setCounter(count => count + 1)
+ setInterval(() => {
+  setCounter(count => count + 1)
   }, 1000);
 
-return () => {
-  clearInterval(key);
-}
 
-
-}, [])
 
 
   return (
