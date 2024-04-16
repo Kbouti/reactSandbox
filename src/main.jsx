fa930df0854f338348c312ae4570c96ae997df28
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-04-15 17:06:41 -0700

    Create working shithead function

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { useState, useEffect } from "react";
+
+
+const kevin = {
+  name:"Kevin",
+  isShithead: false,
+}
+
+const bikeThief = {
+  name: "Jerry",
+  "isShithead": true,
+}
+
+
+function Shithead({person}) {
+  if (person.isShithead){
+    return (
+      <div>{person.name} is a shithead.</div>
+    )
+  } else {
+    return (
+      <div>{person.name} is not a shithead. Nice!</div>
+    )
+  }
+}
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Shithead person =  {kevin}/>
     <App />
   </React.StrictMode>,
 )
