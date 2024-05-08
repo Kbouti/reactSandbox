9100d3c44a1483aa2e5c55a41f9189550fa9783d
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-05-08 10:01:39 -0700

    Create src/count file

---
+++ src/count.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Count extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            // So where is this going to get the todos from?
+        }
+    }
+}
