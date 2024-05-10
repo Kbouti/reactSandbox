86bd4b130b869a0b9f972772da19d1377af457a2
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-05-10 13:47:41 -0700

    Make ternary declaration and comment next issue

--- src/output.jsx
+++ src/output.jsx
@@ -4,14 +4,14 @@ class Output extends Component {
   constructor(props) {
     super(props);
 
-    // this.state = {
-    //   todo: props.todo,
-    //   editable: props.editable,
-    // };
+    this.state = {
+      todo: props.todo,
+      editable: props.editable,
+    };
 
 
-    this.todo = props.todo;
-    this.editable = props.editable;
+    // this.todo = props.todo;
+    // this.editable = props.editable;
 
     this.logTodo = this.logTodo.bind(this);
   }
@@ -26,16 +26,25 @@ class Output extends Component {
   // How do we pass those properties to it??  With PROPS. But how do we handle props in a class component
 
 //  ****************************************************************************************************
-// OK I think we're getting props now, but the issue may be that we forgot to set the editable property to the original todo when we made it?
+// Ok I think there's a problem with state
+
+// We establish the component and then click edit, but nothing changes in the output component
 //  ****************************************************************************************************
 
 
   render() {
     return (
       <>
-        <div>maFuckin output</div>
+
+
+
+{this.props.editable == true ? <input>input!</input> : <div>{this.props.todo}</div>}
+{/* I think this ternary statement is working, we're just not altering state like we want to */}
+
+
+
         <button onClick={this.logTodo}>output</button>
-        <div>this.props.todo: {this.props.todo}</div>
+        {/* <div>this.props.todo: {this.props.todo}</div> */}
         <div>this.props.editable: {this.props.editable}</div>
 
       </>
