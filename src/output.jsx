c7cc64a26172081c9640cbd484912a4eca480aea
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-05-11 08:24:34 -0700

    Edit pseudocode for next steps

--- src/output.jsx
+++ src/output.jsx
@@ -9,7 +9,6 @@ class Output extends Component {
       editable: props.editable,
     };
 
-
     // this.todo = props.todo;
     // this.editable = props.editable;
 
@@ -21,32 +20,30 @@ class Output extends Component {
     console.log(`this.editable: ${this.props.editable}`);
   }
 
-  // HOW THE FUCK DO I PASS MY TODOS TO THE CHILD COMPONENT???
+  //  ****************************************************************************************************
+  // Ok I think there's a problem with state
   // This component will render a single todo component. Thus, it only needs to know it's todo value and whether it is selected or not.
-  // How do we pass those properties to it??  With PROPS. But how do we handle props in a class component
 
-//  ****************************************************************************************************
-// Ok I think there's a problem with state
+  // We establish the component and then click edit, but nothing changes in the output component
 
-// We establish the component and then click edit, but nothing changes in the output component
-//  ****************************************************************************************************
+  //  I think we gotta raise state?
+  // We need to pass the whole todos list to the output component. Not just a single one. The individual output components need access to the whole listso that they can delete themselves from the list.
 
+  //  ****************************************************************************************************
 
   render() {
     return (
       <>
-
-
-
-{this.props.editable == true ? <input>input!</input> : <div>{this.props.todo}</div>}
-{/* I think this ternary statement is working, we're just not altering state like we want to */}
-
-
+        {this.props.editable == true ? (
+          <input>input!</input>
+        ) : (
+          <div>{this.props.todo}</div>
+        )}
+        {/* I think this ternary statement is working, we're just not altering state like we want to */}
 
         <button onClick={this.logTodo}>output</button>
         {/* <div>this.props.todo: {this.props.todo}</div> */}
         <div>this.props.editable: {this.props.editable}</div>
-
       </>
     );
   }
