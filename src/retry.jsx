b1eae75a41d5ac222709ef7f0551c34e483361e8
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-05-13 12:40:23 -0700

    Complete delete button logic

--- src/retry.jsx
+++ src/retry.jsx
@@ -5,8 +5,8 @@ export default class InputRetry extends Component {
     super(props);
 
     this.state = {
-        todos: [],
-        inputVal: ""
+      todos: [],
+      inputVal: "",
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
@@ -14,48 +14,53 @@ export default class InputRetry extends Component {
     this.handleDelete = this.handleDelete.bind(this);
   }
 
-
-handleInputChange(e){
+  handleInputChange(e) {
     this.setState((state) => ({
-        ...state,
-        inputVal: e.target.value,
+      ...state,
+      inputVal: e.target.value,
     }));
-}
+  }
 
-handleSubmit(e){
+  handleSubmit(e) {
     e.preventDefault();
 
     // Ok, here's where we gotta deviate from the script. Instead of just adding the string title of the todo, each one needs to be an object so we can store the editable boolean we'll need for the final exercise.
-const newEntry = {
-    todo: this.state.inputVal,
-    editable: "false"
-}
+    const newEntry = {
+      todo: this.state.inputVal,
+      editable: "false",
+    };
 
-const newArray = [];
-for (let i = 0; i < this.state.todos.length;i++)[
-    newArray.push(this.state.todos[i])
-]
-newArray.push(newEntry);
+    const newArray = [];
+    for (let i = 0; i < this.state.todos.length; i++)
+      [newArray.push(this.state.todos[i])];
+    newArray.push(newEntry);
 
-this.setState({
-    todos: newArray,
-    inputVal: ""
-})
+    this.setState({
+      todos: newArray,
+      inputVal: "",
+    });
+  }
 
+  handleDelete(e) {
+    const targetTodo = e.target.parentElement.firstChild.innerHTML;
+    console.log(`delete function called for targetTodo: ${targetTodo}`);
 
+    const newArray = [];
+    for (let i = 0; i < this.state.todos.length; i++) {
+      if (this.state.todos[i].todo !== targetTodo) {
+        newArray.push(this.state.todos[i]);
+      }
+    }
 
-}
-
-handleDelete(e){  
-}
-
+    this.setState((state) => ({
+      todos: newArray,
+      inputVal: this.state.inputVal,
+    }));
+  }
 
   render() {
     return (
       <section>
-        {/* <p>Trying this again</p>
-        <h3>{this.props.name}</h3> */}
-
         <form autoComplete="off" onSubmit={this.handleSubmit}>
           <label htmlFor="task-entry">Enter a task: </label>
           <input
@@ -68,10 +73,20 @@ handleDelete(e){
         </form>
         <h4>Task count: {this.state.todos.length}</h4>
         <ul>
+          {/* Here we'll need to map through todos and conditionally render */}
 
-{/* Here we'll need to map through todos and conditionally render */}
-
-
+          {this.state.todos.map((entry) => (
+            <li key={entry.todo}>
+              <p className="todo">{entry.todo}</p>
+              <button
+                onClick={this.handleDelete}
+                className="deleteButton"
+                type="delete"
+              >
+                Delete
+              </button>
+            </li>
+          ))}
         </ul>
       </section>
     );
