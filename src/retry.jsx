5eac65f936edb014f830f6ea85f658dae8a51a3a
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-05-13 12:14:45 -0700

    Implement count feature and submit new todo

--- src/retry.jsx
+++ src/retry.jsx
@@ -26,11 +26,22 @@ handleSubmit(e){
     e.preventDefault();
 
     // Ok, here's where we gotta deviate from the script. Instead of just adding the string title of the todo, each one needs to be an object so we can store the editable boolean we'll need for the final exercise.
+const newEntry = {
+    todo: this.state.inputVal,
+    editable: "false"
+}
 
+const newArray = [];
+for (let i = 0; i < this.state.todos.length;i++)[
+    newArray.push(this.state.todos[i])
+]
+newArray.push(newEntry);
 
-    this.setState((state) => ({
-        todos: stat
-    }))
+this.setState({
+    todos: newArray,
+    inputVal: ""
+})
+
 
 
 }
@@ -55,7 +66,7 @@ handleDelete(e){
           />
           <button type="submit">Submit</button>
         </form>
-        <h4>All the tasks! </h4>
+        <h4>Task count: {this.state.todos.length}</h4>
         <ul>
 
 {/* Here we'll need to map through todos and conditionally render */}
